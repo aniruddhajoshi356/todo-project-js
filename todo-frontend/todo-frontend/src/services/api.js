c765f2ab47e10d3aa34ec5ac518cc9e19a37c7eb
Aniruddha Joshi <aniruddha.joshi@idfy.com>
2026-02-19 21:25:03 +0530

    Updated VITE_API_URL

--- todo-frontend/todo-frontend/src/services/api.js
+++ todo-frontend/todo-frontend/src/services/api.js
@@ -1,5 +1,5 @@
 //const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 
 export const fetchTodos = async (page = 1, search = "", filter = "ALL") => {
